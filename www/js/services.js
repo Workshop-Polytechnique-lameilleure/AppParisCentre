8d664e2f066c1401b9fb2c1828ddd61577ff5ecc
audreybench3 <audrey.benchaya@gmail.com>
2016-10-20 18:04:37 +0200

    il faut finir la page meeting et ajouter le modal pour créer un nouveau meeting et la page account

--- www/js/services.js
+++ www/js/services.js
@@ -8,13 +8,13 @@ angular.module('starter.services', [])
   latitude: 48.86751,
   longitude: 2.363777,
   participants: "Paul, Pierre, Jacques",
-  date: DateTime.new(2016,12,31,23,59,59)
+  date: "2016-11-10"
    }, {
   id_meeting: "Tournée générale d'aspirine",
   latitude: 48.852969,
   longitude: 2.349873,
   participants: "Jacqui Michel",
-  date: DateTime.new(2017,01,01,14,00,00)
+  date: "2016-11-10"
   }];
 
   return {
